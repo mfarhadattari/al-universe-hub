8cd83a011d6b0a7763582e418dc5213fdabb6abb
Moammad Farhad <mfarhadattari>
2023-03-03 11:51:07 +0600

    details card img size modify, title, descreption and accuracy data set dynamicaly

--- js/app.js
+++ js/app.js
@@ -149,6 +149,7 @@ const displayAllData = (toolsData) => {
 };
 
 const loadDetails = (id) => {
+  document.getElementById("modal-loader").classList.remove("hidden");
   url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
   fetch(url)
     .then((res) => res.json())
@@ -157,68 +158,64 @@ const loadDetails = (id) => {
 
 // details btn
 const displayDetails = (toolData) => {
-  console.log(toolData);
-  const modalContainer = document.getElementById("modal-container");
-  const modalDiv = document.createElement("div");
-  modalDiv.innerHTML = `
-  <!-- Details Modal -->
-  <input type="checkbox" id="my-modal-3" class="modal-toggle" />
-  <div class="modal">
-      <label for="my-modal-3" class="btn btn-circle bg-red-500 absolute z-20 right-20 top-7">✕</label>
-      <div class="modal-box w-11/12 max-w-5xl z-0">
-          <div class="flex justify-between gap-5">
-              <div class="card w-1/2 bg-red-50 shadow-xl p-4 border-2 border-red-300">
-                  <h1 class="text-2xl font-medium">
-                      ChatGPT is an AI-powered chatbot platform that uses OpenAI's GPT technology to
-                      simulate
-                      human conversation.
-                  </h1>
-                  <div class="card-body p-1">
-                      <div class="flex gap-4 text-lg text-center my-3">
-                          <div class="bg-white p-5 text-green-500 rounded-xl">$10/month Basic</div>
-                          <div class="bg-white p-5 text-orange-500 rounded-xl">$50/month Pro</div>
-                          <div class="bg-white p-5 text-pink-500 rounded-xl">Contact us Enterprise</div>
-                      </div>
-                      <div class="flex gap-5 items-center justify-between">
-                          <div class="">
-                              <h2 class="text-xl font-semibold">Features</h2>
-                              <ul class="list-disc pl-5">
-                                  <li>Customizable responses</li>
-                                  <li>Multilingual support</li>
-                                  <li>Seamless integration</li>
-                              </ul>
-                          </div>
-                          <div>
-                              <h2 class="text-xl font-semibold">Integrations</h2>
-                              <ul class="list-disc pl-5">
-                                  <li>FB Messenger</li>
-                                  <li>Slack</li>
-                                  <li>Telegram</li>
-                              </ul>
-                          </div>
-                      </div>
-                  </div>
+  console.log(Array.isArray(toolData.pricing) ? toolData.pricing[0] : 'Not Pricing Data Found');
+  const cardBody = document.getElementById("card-body");
+  cardBody.innerHTML = "";
+  cardBody.innerHTML = `
+  <div class="flex justify-between gap-5">
+  <div class="card w-1/2 bg-red-50 shadow-xl p-4 border-2 border-red-300">
+      <h1 class="text-2xl font-medium">
+          ${toolData.description}
+      </h1>
+      <div class="card-body p-1">
+          <div class="flex gap-4 text-lg text-center my-3">
+              <div class="bg-white p-5 text-green-500 rounded-xl">
+               ${Array.isArray(toolData.pricing) ? `${toolData.pricing[0].price} ${toolData.pricing[0].plan}` : 'Not Pricing Data Found'}
               </div>
-              <div class="card w-1/2 bg-base-100 shadow-xl">
-
-                  <div class="w-full">
-                      <span class=" badge badge-secondary absolute right-1 top-1">
-                          94% accuracy
-                      </span>
-                      <figure class="">
-                          <img src="https://img.olhardigital.com.br/wp-content/uploads/2023/01/chatgpt_assistente.jpg"
-                              alt="Shoes" class="rounded-xl w-full" />
-                      </figure>
-                  </div>
-
-                  <div class="card-body items-center text-center">
-                      <h2 class="card-title">Hi, how are you doing today?</h2>
-                      <p>I'm doing well, thank you for asking. How can I assist you today?</p>
-                  </div>
+              <div class="bg-white p-5 text-orange-500 rounded-xl">
+              ${Array.isArray(toolData.pricing) ? `${toolData.pricing[1].price} ${toolData.pricing[1].plan}` : 'Not Pricing Data Found'}
+              </div>
+              <div class="bg-white p-5 text-pink-500 rounded-xl">
+              ${Array.isArray(toolData.pricing) ? `${toolData.pricing[2].price} ${toolData.pricing[2].plan}` : 'Not Pricing Data Found'}
+              </div>
+          </div>
+          <div class="flex gap-5 items-center justify-between">
+              <div class="">
+                  <h2 class="text-xl font-semibold">Features</h2>
+                  <ul class="list-disc pl-5">
+                      <li>Customizable responses</li>
+                      <li>Multilingual support</li>
+                      <li>Seamless integration</li>
+                  </ul>
+              </div>
+              <div>
+                  <h2 class="text-xl font-semibold">Integrations</h2>
+                  <ul class="list-disc pl-5">
+                      <li>FB Messenger</li>
+                      <li>Slack</li>
+                      <li>Telegram</li>
+                  </ul>
               </div>
           </div>
       </div>
   </div>
+  <div class="card w-1/2 bg-base-100 shadow-xl">
+
+      <div class="w-full">
+          <span class=" badge badge-secondary absolute right-24 top-1">
+              ${(typeof(toolData.accuracy.score) == 'number') ? `${toolData.accuracy.score * 100}% accuracy` : 'Accuracy Not Found'}
+          </span>
+          <figure class="">
+              <img src="${toolData.image_link[0]}" alt="${toolData.tool_name}" class="rounded-xl w-72  h-40" />
+          </figure>
+      </div>
+
+      <div class="card-body items-center text-center">
+          <h2 class="card-title">${Array.isArray(toolData.input_output_examples) ? toolData.input_output_examples[0].input : 'No Data Found' }</h2>
+          <p>${Array.isArray(toolData.input_output_examples) ? toolData.input_output_examples[0].output : 'No Data Found' }</p>
+      </div>
+  </div>
+</div>
   `;
-  modalContainer.appendChild(modalDiv);
+  document.getElementById("modal-loader").classList.add("hidden");
 };
